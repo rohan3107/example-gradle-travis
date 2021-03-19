4927b63c0c1ea1ceb6596c3a6fc6093ffbdf8f4e
rohan3107 <rohansood15@gmail.com>
2021-03-19 22:57:26 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
 'use strict';
 
+bcrypt.hash(yourPassword, saltRounds, (err, hash) => {
+  // Now we can store the password hash in db.
+});
+
 var preTax = parseInt(req.body.preTaxes);
 var afterTax = JSON.parse(someJSON);
